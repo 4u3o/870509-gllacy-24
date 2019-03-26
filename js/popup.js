4157e7d6cf3083f3d62d2686005a0c677d9f5481
4u3o <radjahair@gmail.com>
2019-03-26 17:04:15 +0300

    js magic

---
+++ js/popup.js
@@ -0,0 +1,46 @@
+var link = document.querySelector(".modal-link");
+var popup = document.querySelector(".modal-lining");
+var close = document.querySelector(".modal-close");
+var imya = document.querySelector(".feedback-form-name");
+
+var form = popup.querySelector(".feedback-form");
+var message = popup.querySelector(".feedback-form-text");
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+    storage = localStorage.getItem("message");
+} catch (err) {
+     isStorageSupport = false;
+}
+
+link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.add("modal-show");
+    imya.focus();
+});
+
+close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal-show");
+    popup.classList.remove("modal-error");
+});
+
+form.addEventListener("submit", function (evt) {
+    if (!message.value) {
+        evt.preventDefault();
+        popup.classList.remove("modal-error");
+        popup.offsetWidth = popup.offsetWidth;
+        popup.classList.add("modal-error");
+    }
+  });
+
+document.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+        evt.preventDefault();
+        if (popup.classList.contains("modal-show")) {
+            popup.classList.remove("modal-show");
+        }
+    }
+});
